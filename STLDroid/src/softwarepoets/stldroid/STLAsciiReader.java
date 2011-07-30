58bd621c04a1ede972c7089b2df1a4b90024f201
Markus Klink <justjoheinz@gmail.com>
2011-07-30 22:55:03 +0200

    zoom pinch

--- STLDroid/src/softwarepoets/stldroid/STLAsciiReader.java
+++ STLDroid/src/softwarepoets/stldroid/STLAsciiReader.java
@@ -1,46 +1,140 @@
 package softwarepoets.stldroid;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.StreamTokenizer;
 
+import toxi.geom.Vec3D;
 import toxi.geom.mesh.Mesh3D;
+import toxi.geom.mesh.TriangleMesh;
+import android.util.Log;
 
 public class STLAsciiReader {
-	
-	private StreamTokenizer tokenizer;
 
-	private Mesh3D mesh;
+	private StreamTokenizer t;
+
+	private Mesh3D tmpMesh;
+
+	private ParserState state = new HeadState();
+
+	public Mesh3D load(String fileName) throws FileNotFoundException {
+		return load(new FileInputStream(fileName));
+	}
+
+	public Mesh3D load(InputStream fis) {
+		tmpMesh = new TriangleMesh();
+		Log.i("STLAsciiReader", "parsing...");
+		t = new StreamTokenizer(fis);
+		t.resetSyntax();
+		t.wordChars(33, 126);
+		t.whitespaceChars(0, 32);
+		
+		while ( state != null) {
+			Log.i("STLAsciiReader", "parsing...");
+			state = state.next();
+		}
+
+		return tmpMesh;
+
+	}
+
 	
-	interface ParserState {
+
+	public interface ParserState {
 		ParserState next() throws IllegalStateException;
 	}
-	
-	class HeadState implements ParserState {
+
+	public class HeadState implements ParserState {
 
 		@Override
 		public ParserState next() throws IllegalStateException {
-			// TODO Auto-generated method stub
-			return null;
+			try {
+				// solid
+				t.nextToken();
+				if (!"solid".equalsIgnoreCase(t.sval))
+					throw new IllegalStateException(t.sval);
+				// name
+				t.nextToken();
+				return new FacetState();
+			} catch (IOException e) {
+				throw new IllegalStateException(e.getMessage());
+			}
 		}
-		
 	}
-	
+
 	class FacetState implements ParserState {
-
 		@Override
 		public ParserState next() throws IllegalStateException {
-			// TODO Auto-generated method stub
-			return null;
+			try {
+				t.nextToken();
+				if (!"facet".equalsIgnoreCase(t.sval))
+					throw new IllegalStateException(t.sval);
+				t.nextToken();
+				if (!"normal".equalsIgnoreCase(t.sval))
+					throw new IllegalStateException(t.sval);
+				t.nextToken();
+				Log.i("STLAsciiReader", "Token:" + t.sval);
+				t.nextToken();
+				Log.i("STLAsciiReader", "Token:" + t.sval);
+				t.nextToken();
+				Log.i("STLAsciiReader", "Token:" + t.sval);
+				// vertex
+				t.nextToken();
+				if (!"outer".equalsIgnoreCase(t.sval))
+					throw new IllegalStateException(t.sval);
+				t.nextToken();
+				Log.i("STLAsciiReader", "Token:" + t.sval);
+				if (!"loop".equalsIgnoreCase(t.sval))
+					throw new IllegalStateException(t.sval);
+				float[] vertex = new float[3];
+				Vec3D[] vector = new Vec3D[3];
+				for (int j = 0; j < 3; j++) {
+					t.nextToken();
+					if (!"vertex".equalsIgnoreCase(t.sval))
+						throw new IllegalStateException(t.sval);
+					t.nextToken();
+					vertex[0] = Float.parseFloat(t.sval);
+					t.nextToken();
+					vertex[1] = Float.parseFloat(t.sval);
+					t.nextToken();
+					vertex[2] = Float.parseFloat(t.sval);
+					vector[j] = new Vec3D(vertex);
+				}
+				tmpMesh.addFace(vector[0], vector[2], vector[1]);
+				t.nextToken();
+				if (!"endloop".equalsIgnoreCase(t.sval))
+					throw new IllegalStateException(t.sval);
+				t.nextToken();
+				if (!"endfacet".equalsIgnoreCase(t.sval))
+					throw new IllegalStateException(t.sval);
+				
+				// next state transition
+				t.nextToken();
+				if ("facet".equalsIgnoreCase(t.sval)) {
+					t.pushBack();
+					return new FacetState();
+				}
+				if ("endsolid".equalsIgnoreCase(t.sval)) {
+					t.pushBack();
+					return new FinalState();
+				}
+				throw new IllegalStateException();
+			} catch (IOException e) {
+				throw new IllegalStateException(e.getMessage());
+			} catch (NumberFormatException e) {
+				throw new IllegalStateException(e.getMessage());
+			}
 		}
-		
 	}
-	
+
 	class FinalState implements ParserState {
 
 		@Override
 		public ParserState next() throws IllegalStateException {
-			// TODO Auto-generated method stub
 			return null;
 		}
-		
+
 	}
 }
--- STLDroid/src/softwarepoets/stldroid/STLView.java
+++ STLDroid/src/softwarepoets/stldroid/STLView.java
@@ -2,20 +2,28 @@ package softwarepoets.stldroid;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.concurrent.ExecutionException;
 
 import processing.core.PApplet;
+import processing.core.PShape;
 import processing.core.PShape3D;
+import toxi.color.TColor;
+import toxi.geom.mesh.Mesh3D;
+import toxi.processing.ToxiclibsSupport;
 import android.app.ProgressDialog;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
 public class STLView extends PApplet {
 
@@ -39,6 +47,7 @@ public class STLView extends PApplet {
 				// filesystem
 				out = new BufferedOutputStream(fos);
 				conn = url.openConnection();
+
 				in = conn.getInputStream();
 
 				// Get the data
@@ -75,56 +84,103 @@ public class STLView extends PApplet {
 		@Override
 		protected void onPostExecute(Uri result) {
 			pd.dismiss();
-			downloadRequired = false;
 		}
 
 		@Override
 		protected void onPreExecute() {
 			pd = ProgressDialog.show(STLView.this, null, "Downloading");
-			downloadRequired = true;
 		}
 
 	}
 
-	private PShape3D stl;
+	private static final String TAG = "STLVIEW";
+
+	public static final int GROW = 0;
+	public static final int SHRINK = 1;
+
+	public static final int TOUCH_INTERVAL = 0;
+
+	public static final float MIN_SCALE = 1f;
+	public static final float MAX_SCALE = 4f;
+	public static final float ZOOM = 1.5f;
+
+	private PShape stl;
 	private float rotateX;
+	private Mesh3D mesh;
+	private ToxiclibsSupport toxic;
+
+	private Download downloader;
 
 	private float rotateY;
-	private boolean downloadRequired;
+	private Uri fileUri;
+
+	float scale = 1.0f;
+
+	float xCur, yCur, xPre, yPre, xSec, ySec, distDelta, distCur, distPre = -1;
+	int mWidth, mHeight, mTouchSlop;
+	long mLastGestureTime;
 
 	@Override
 	public void draw() {
 		background(0);
+		initlights();
 		translate(screenWidth / 2, screenHeight / 2);
 		rotateX(rotateX);
 		rotateY(rotateY);
-		box(40, 40, 40);
+		scale(scale);
+		toxic.chooseStrokeFill(false, TColor.newGray(150), TColor.newGray(150));
+		toxic.mesh(mesh, false);
 	}
 
-	@Override
-	public void mouseDragged() {
-		rotateX = map(mouseX, 0, screenWidth, 0, TWO_PI);
-		rotateY = map(mouseY, 0, screenHeight, 0, TWO_PI);
+	private void initlights() {
+		directionalLight(200, 200, 200, 0, 0, 1);
+		directionalLight(200, 200, 200, 1, 1, 0);
+		directionalLight(200, 200, 200, -1, 1, -10);
 	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		Uri filename = getIntent().getData();
-		/**
-		if ("http".equals(filename.getScheme())) {
-			new Download().execute(filename);
+		fileUri = getIntent().getData();
+		Log.i(TAG, fileUri.toString());
+		if ("http".equals(fileUri.getScheme())) {
+			downloader = new Download();
+			downloader.execute(fileUri);
 		}
-		if (".obj".equalsIgnoreCase(filename.getLastPathSegment())) {
-			stl = new PShape3D(STLView.this, filename.toString(), null);
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		if (downloader != null) {
+			try {
+				fileUri = downloader.get();
+			} catch (InterruptedException e) {
+				Log.e(TAG, e.getMessage());
+			} catch (ExecutionException e) {
+				Log.e(TAG, e.getMessage());
+			} finally {
+				downloader = null;
+			}
 		}
-		*/
+		Log.i(TAG, "filename: " + fileUri.getPath());
+		Log.i(TAG, "filename: " + fileUri.getPath());
+		if (fileUri.getLastPathSegment().endsWith(".stl")) {
+			try {
+				Log.i(TAG, "STL File");
+				mesh = new STLAsciiReader().load(fileUri.getPath());
+			} catch (FileNotFoundException e) {
+				Log.e(TAG, e.getMessage());
+			} catch (IllegalStateException e) {
+				Toast.makeText(STLView.this, "Not an ASCII STL file",
+						Toast.LENGTH_LONG).show();
+			}
+		}
 	}
 
 	@Override
 	public void setup() {
-		// stl = loadShape(filename)
+		toxic = new ToxiclibsSupport(this);
 	}
 
 	@Override
@@ -134,7 +190,7 @@ public class STLView extends PApplet {
 
 	@Override
 	public String sketchRenderer() {
-		return P3D;
+		return OPENGL;
 	}
 
 	@Override
@@ -142,4 +198,71 @@ public class STLView extends PApplet {
 		return screenWidth;
 	}
 
+	@Override
+	public boolean surfaceTouchEvent(MotionEvent event) {
+		Log.i(TAG, "surfachtouchevent");
+		int action = event.getAction() & MotionEvent.ACTION_MASK, p_count = event
+				.getPointerCount();
+
+		switch (action) {
+		case MotionEvent.ACTION_MOVE:
+			Log.i(TAG, "move");
+			// point 1 coords
+			xCur = event.getX(0);
+			yCur = event.getY(0);
+			if (p_count == 1) {
+				rotateY = map(xCur, 0, screenWidth, TWO_PI, 0);
+				rotateX = map(yCur, 0, screenHeight, TWO_PI, 0);
+			}
+			if (p_count > 1) {
+				Log.i(TAG, "move pcount >2");
+				// point 2 coords
+				xSec = event.getX(1);
+				ySec = event.getY(1);
+
+				// distance between
+				distCur = (float) Math.sqrt(Math.pow(xSec - xCur, 2)
+						+ Math.pow(ySec - yCur, 2));
+				distDelta = distPre > -1 ? distCur - distPre : 0;
+				Log.i(TAG, "distDelta: " + distDelta);
+				float scale = this.scale;
+				long now = android.os.SystemClock.uptimeMillis();
+				if (Math.abs(distDelta) > mTouchSlop) {
+					mLastGestureTime = 0;
+
+					int mode = distDelta > 0 ? GROW : (distCur == distPre ? 2
+							: SHRINK);
+
+					switch (mode) {
+					case GROW: // grow
+						this.scale = scale + (distDelta * 0.01f);
+						break;
+					case SHRINK: // shrink
+						this.scale = scale + (distDelta * 0.01f);
+						break;
+					}
+					scale = (scale <= 0 ? scale = 0 : scale);
+
+					mLastGestureTime = now;
+					xPre = xCur;
+					yPre = yCur;
+					distPre = distCur;
+					return true;
+				}
+			}
+
+			xPre = xCur;
+			yPre = yCur;
+			distPre = distCur;
+			break;
+		case MotionEvent.ACTION_DOWN:
+		case MotionEvent.ACTION_UP:
+			Log.i(TAG, "updown");
+			distPre = -1;
+			mLastGestureTime = android.os.SystemClock.uptimeMillis();
+			break;
+		}
+
+		return super.surfaceTouchEvent(event);
+	}
 }
