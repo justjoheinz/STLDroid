2cde1c6e132ef8f5811345589b379dd2a200345f
Markus Klink <justjoheinz@gmail.com>
2011-10-09 10:42:59 +0200

    refactoring - added FileExplorer to project

--- STLDroid/src/softwarepoets/stldroid/STLDroid.java
+++ STLDroid/src/softwarepoets/stldroid/STLDroid.java
@@ -36,7 +36,6 @@ public class STLDroid extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
 		preferences = PreferenceManager
 				.getDefaultSharedPreferences(STLDroid.this);
 
